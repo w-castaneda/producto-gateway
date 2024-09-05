75139f6c8656e65b2b7c2bcc4042555975c6acee
Wilfredo Castaneda <wilfredo.castaneda.guerrero@gmail.com>
2024-09-04 18:29:32 -0600

    Crear productos Gateway
    Agregar funcionalidad al gateway
    Modificar el archivo properties y agregar las rutas

---
+++ src/main/java/com/productos/gateway/producto_gateway/ProductoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.productos.gateway.producto_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/productos/gateway/producto_gateway/ProductoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productos.gateway.producto_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
